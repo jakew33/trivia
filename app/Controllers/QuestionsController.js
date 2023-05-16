1a48f0b35352bc6c8a36076c269f8c92a3c8c6d6
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-05-15 19:48:09 -0600

    draws questions to page

--- app/Controllers/QuestionsController.js
+++ app/Controllers/QuestionsController.js
@@ -1,21 +1,25 @@
 import { appState } from "../AppState.js";
 import { questionsService } from "../Services/QuestionsService.js";
 import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js"
 
 
-function drawQuestions()  {
+function _drawQuestions()  {
   console.log('this is from the draw', appState.questions)
   let template = ''
   appState.questions.forEach(q =>{
     template += q.CardTemplate
   })
+  setHTML('questions', template)
 }
 
 export class QuestionsController{
   constructor(){
   console.log('questions controller yay');
-  appState.on('questions', drawQuestions)
+  appState.on('questions', _drawQuestions)
   this.goGetQuestions()
+  // _drawQuestions()
+
   }
 
   async goGetQuestions () {
--- app/Models/Question.js
+++ app/Models/Question.js
@@ -6,7 +6,7 @@ export class Question {
   get CardTemplate () {
     return /*html*/ `
     <div class="col-md-4 col-lg-3 my-3">
-    <p class="card ${this.question}"></p>
+    <p class="card"><b>${this.question}</b></p>
     </div>
     `
   }
